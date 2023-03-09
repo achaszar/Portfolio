ac9b8a40236f739bb6a04dedf827765fea2b6586
Austin Chaszar <awchaszar@gmail.com>
2023-03-08 20:55:35 -0500

    MyPortfolio
    Added some javascript and deleted some html

--- Portfolio/wwwroot/js/site.js
+++ Portfolio/wwwroot/js/site.js
@@ -1,3 +1,7 @@
 ﻿function openGithub() {
     window.open("https://github.com/achaszar");
 }
+
+function openLinkedin() {
+    window.open("https://www.linkedin.com/in/austin-chaszar-778500159/")
+}
