3b26777771708b9a0cf798f0da607fb2ce937d59
Austin Chaszar <awchaszar@gmail.com>
2023-03-12 21:09:36 -0400

    Some changes
    Edited some code

--- Portfolio/wwwroot/js/site.js
+++ Portfolio/wwwroot/js/site.js
@@ -3,5 +3,13 @@
 }
 
 function openLinkedin() {
-    window.open("https://www.linkedin.com/in/austin-chaszar-778500159/")
+    window.open("https://www.linkedin.com/in/austin-chaszar-778500159/");
+}
+
+function myFunction() {
+    navigator.clipboard.writeText(document.getElementById('myInput').innerText)
+        .then(function () {
+            console.log('text has been copied!')
+            alert("Copied!!")
+        })
 }
